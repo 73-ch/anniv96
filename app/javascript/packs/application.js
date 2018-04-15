58f75f781203c3bf8952c7c27a73f90ccdc6c675
nami634 <cigarbox.nami@gmail.com>
2018-04-16 04:15:39 +0900

    メインコンテンツがとりあえず完成

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,16 +11,39 @@ import PreAnimation from "./pre_animation";
 import PreStyle from "./pre_style";
 
 global.THREE = require('./three.min.js');
+global.$ = require('jquery');
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    const pre_animation = new PreAnimation(() => {
-        const style = new PreStyle();
+    const loader = new THREE.TextureLoader();
 
-        window.addEventListener('resize', () => {
-            style.resizeCanvas();
-        });
+    $.getJSON("../wind_data.json", (json_data) => {
+        loader.load(
+            // resource URL
+            './assets/logo-white.png',
+
+            // onLoad callback
+            texture => {
+                const pre_animation = new PreAnimation(texture, json_data, () => {
+                    // const style = new PreStyle();
+
+                    window.addEventListener('resize', () => {
+                        // style.resizeCanvas();
+                        pre_animation.resizeCanvas();
+                    });
+                });
+            },
+
+            // onProgress callback currently not supported
+            undefined,
+
+            // onError callback
+            (err) => {
+                console.error('An error happened.');
+            }
+        );
     });
+
 
     // Three.js Inspector debug
     window.THREE = THREE;
--- app/javascript/packs/pre_animation.js
+++ app/javascript/packs/pre_animation.js
@@ -1,44 +1,40 @@
 'use strict'
 export default class {
-    constructor(_canvas) {
-        this.canvas = _canvas;
+    constructor(texture, json, callback) {
+        this.texture = texture;
+        this.window_size = [window.innerWidth, window.innerHeight];
+
+        this.wind_data = json;
+
+        this.counter = 0;
 
         this.renderer = new THREE.WebGLRenderer({antialias: true});
 
-        this.renderer.setPixelRatio(window.deveicePixelRatio);
-        this.renderer.setSize(window.innerWidth, window.innerHeight);
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.setSize(this.window_size[0], this.window_size[1]);
 
-        this.scene = new THREE.Scene();
+        this.logoLayer();
 
-        this.camera = new THREE.PerspectiveCamera(45, 1920 / 1112, 1, 10000);
-        this.camera.position.set(0, 0, 100);
-
-
-        const geometry = new THREE.BoxGeometry(10, 10, 10);
-        const material = new THREE.MeshStandardMaterial({color: 0x0000FF});
-        this.box = new THREE.Mesh(geometry, material);
-        this.scene.add(this.box);
-
-        this.light = new THREE.DirectionalLight(0xffffff);
-        this.light.position.set(1, 1, 1);
-
-        this.scene.add(this.light);
-
-        this.renderer.render(this.scene, this.camera);
+        this.layerBlend();
 
         const wrapper = document.querySelector(".top-content");
         wrapper.appendChild(this.renderer.domElement);
 
         this.update();
 
+        callback();
+
         // Three.js Inspector debug
-        window.scene = this.scene;
+        window.scene = this.final.scene;
+
+        window.scene = this.logo.scene;
     }
-
     update() {
-        this.box.rotation.x += 0.01;
-        this.box.rotation.y += 0.01;
+        this.final.material.uniforms.wind.value = new THREE.Vector2(this.wind_data[this.counter].direction, this.wind_data[this.counter].velocity)
 
+        this.counter++;
+
+        if (this.counter >= this.wind_data.length) this.counter = 0;
 
         this.draw(() => {
             requestAnimationFrame(this.update.bind(this));
@@ -46,11 +42,97 @@ export default class {
     }
 
     draw(callback) {
-        this.renderer.render(this.scene, this.camera);
+        this.renderer.render(this.logo.scene, this.logo.camera, this.logo.rt);
 
+        // this.renderer.render(this.logo.scene, this.logo.camera);
+
+        this.final.material.uniforms['texture'].value = this.logo.rt.texture;
+
+        this.renderer.render(this.final.scene, this.final.camera);
+        // console.log(this.logo.rt.texture);
         callback();
     }
 
+    logoLayer() {
+        this.logo = {};
+        this.logo.rt = new THREE.WebGLRenderTarget(this.window_size[0], this.window_size[1], {
+            minFilter: THREE.LinearFilter,
+            magFilter: THREE.NearestFilter,
+            format: THREE.RGBFormat
+        });
+
+
+        this.logo.scene = new THREE.Scene();
+
+        this.logo.camera = new THREE.PerspectiveCamera(45, this.window_size[0] / this.window_size[1], 1, 10000);
+        this.logo.camera.position.set(0, 0, 100);
+
+        this.createLogo();
+
+        this.logo.light = new THREE.DirectionalLight(0xffffff);
+        this.logo.light.position.set(1, 1, 1);
+
+        this.logo.scene.add(this.logo.light);
+    }
+
+
+    createLogo() {
+        let plane_geometry = new THREE.PlaneGeometry(30, 30);
+        let plane_material = new THREE.MeshBasicMaterial({
+            map: this.texture
+        });
+
+        this.logo.obj = new THREE.Mesh(plane_geometry, plane_material);
+
+        console.log(this.logo.obj);
+
+        this.logo.scene.add(this.logo.obj);
+    }
+
+    layerBlend() {
+        this.final = {};
+
+        this.final.scene = new THREE.Scene();
+
+        this.final.camera = new THREE.PerspectiveCamera(45, this.window_size[0] / this.window_size[1], 1, 10000);
+        this.final.camera.position.set(0, 0, 1000);
+
+        this.final.material = new THREE.ShaderMaterial({
+            fragmentShader: document.getElementById('fs').innerHTML,
+            vertexShader: document.getElementById('vs').innerHTML,
+            uniforms: {
+                texture: {
+                    type: "t",
+                    value: this.logo.rt.texture
+                },
+                wind: {
+                    type: "v2",
+                    value: new THREE.Vector2(this.wind_data[0].direction, this.wind_data[0].velocity)
+                }
+            }
+        });
+
+        this.final.geometry = new THREE.PlaneGeometry( this.window_size[0], this.window_size[1], 1, 1 );
+        this.final.plane = new THREE.Mesh( this.final.geometry, this.final.material );
+
+        this.final.scene.add(this.final.plane);
+    }
+
+    resizeCanvas() {
+        this.window_size = [window.innerWidth, window.innerHeight];
+
+        this.renderer.setPixelRatio(window.devicePixelRatio);
+        this.renderer.setSize(this.window_size[0], this.window_size[1]);
+
+        this.logo.rt.setSize(this.window_size[0], this.window_size[1]);
+
+        console.log(this.logo.rt.width);
+
+        this.logo.camera.aspect = this.window_size[0]/ this.window_size[1];
+        this.logo.camera.updateProjectionMatrix();
+        this.final.camera.aspect = this.window_size[0]/ this.window_size[1];
+        this.final.camera.updateProjectionMatrix();
+    }
 
 }
 
